f9e065c7f2b78056a639f2ebb9138c3030b555a1
HT256 <recke.yanik@gmail.com>
2022-03-30 16:00:44 +0200

    Adjustments

--- TextBoxSnippet/src/de/yanik/programm/Frame.java
+++ TextBoxSnippet/src/de/yanik/programm/Frame.java
@@ -5,6 +5,7 @@ import java.awt.FlowLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JTextField;
@@ -21,38 +22,70 @@ import javax.swing.JTextField;
 public class Frame extends JFrame implements ActionListener{
 	private static final long serialVersionUID = 1L;
 
+	private char maxC;
+	private int max;
 	private JTextField[] textFields;
 	private JLabel[] textLabels;
 	
+	private JButton finish;
+	
 
-	public Frame() {
+	public Frame(int max) {
+		this.setTitle("Input window for neighbours");
+		this.max = max;
+		
+		switch(max) {
+		case 1: maxC = 'A';
+		break;
+		
+		case 2: maxC = 'B';
+		break;
+		
+		case 3: maxC = 'C';
+		break;
+		
+		case 4: maxC = 'D';
+		break;
+		
+		case 5: maxC = 'E';
+		break;
+		
+		case 6: maxC = 'F';
+		break;
+		}
 		
 		this.setLayout(new FlowLayout());
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		this.setResizable(false);
-		this.setPreferredSize(new Dimension(660, 100));
+		this.setPreferredSize(new Dimension(max * 110, 140));
 		
 		// TODO change so it does not leave ~60 slots empty
-		textFields = new JTextField['G'];
+		textFields = new JTextField[maxC + 1];
 		
-		textLabels = new JLabel[6];
+		textLabels = new JLabel[max];
 		
 		// TODO obergrenze variabel machen
-		int i = 0;
-		for(char c = 'A'; c <= 'F'; c++)
+		char ch = 'A';
+		for(int i = 0; i < max; i++)
 		{
-			textLabels[i] = new JLabel("Neighbours of " + c);
+			textLabels[i] = new JLabel("Neighbours of " + ch);
 			textLabels[i].setPreferredSize(new Dimension(100, 20));
 			this.add(textLabels[i]);
-			i++;
+			ch++;
 		}
 		
 		// TODO Obergrenze variable machen
-		for(char c = 'A'; c <= 'F'; c++)
+		for(char c = 'A'; c <= maxC; c++)
 		{
 			textFields[c] = textFieldCreator(c);
 			this.add(textFields[c]);
 		}
+		
+		finish = new JButton("Done");
+		finish.setPreferredSize(new Dimension(100, 35));
+		finish.setVisible(true);
+		finish.addActionListener(this);
+		this.add(finish);
 		
 		this.pack();
 		this.setVisible(true);
@@ -61,7 +94,7 @@ public class Frame extends JFrame implements ActionListener{
 	
 	private boolean onEnter(char tbChar)
 	{
-		if(checkInput(textFields[tbChar].getText(), 3, tbChar))
+		if(checkInput(textFields[tbChar].getText(), max, tbChar))
 		{
 			completeOtherFields(textFields[tbChar].getText(), tbChar);
 			return true;
@@ -75,34 +108,54 @@ public class Frame extends JFrame implements ActionListener{
 	public void actionPerformed(ActionEvent e) {
 		boolean inputCorrect = true;
 		
-		if(e.getSource() == textFields['A']) 
+		if(max >= 1 && e.getSource() == textFields['A']) 
 			if(!onEnter('A'))
 				inputCorrect = false;
 			
 			
-		if(e.getSource() == textFields['B']) 
+		if(max >= 2 && e.getSource() == textFields['B']) 
 			if(!onEnter('B'))
 				inputCorrect = false;
 			
 			
-		if(e.getSource() == textFields['C']) 
+		if(max >= 3 && e.getSource() == textFields['C']) 
 			if(!onEnter('C'))
 				inputCorrect = false;
 			
 			
-		if(e.getSource() == textFields['D']) 
+		if(max >= 4 && e.getSource() == textFields['D']) 
 			if(!onEnter('D'))
 				inputCorrect = false;
 		
 		
-		if(e.getSource() == textFields['E']) 
+		if(max >= 5 && e.getSource() == textFields['E']) 
 			if(!onEnter('E'))
 				inputCorrect = false;
 		
 		
-		if(e.getSource() == textFields['F']) 
+		if(max >= 6 && e.getSource() == textFields['F']) 
 			if(!onEnter('F'))
 				inputCorrect = false;
+		
+		
+		if(e.getSource() == finish) {
+			for(char c = 'A'; c <= maxC; c++)
+				if(!onEnter(c))
+					inputCorrect = false;
+			
+			if(inputCorrect)
+			{
+				;//TODO content der textboxen übertragen
+				try {
+					Thread.sleep(500);		// simulated delay
+				} catch (Exception e2) {
+					// TODO: handle exception
+				}
+				this.dispose();
+			}
+			else
+				System.out.println("Konnte nicht beenden, Fehler in einer der Textboxen");
+		}
 			
 
 		// Am Ende prüfen, außerhalb, da sonst Dopplug
@@ -111,7 +164,7 @@ public class Frame extends JFrame implements ActionListener{
 			System.out.println("Input bitte überprüfen. Mögliche Fehlerquellen:");
 			System.out.println(" - Es gibt gar nicht so viele Ecken/Knoten");
 			System.out.println(" - Irgendwo sind Leerzeichen");
-			System.out.println(" - Ein Buchstabe größer als 'F' wurde eingegeben");
+			System.out.println(" - Ein Buchstabe größer als " + maxC + " wurde eingegeben");
 		}
 		
 	}
@@ -133,7 +186,7 @@ public class Frame extends JFrame implements ActionListener{
 		// Falls ein Buchstabe größer als 'F' eingegeben wurde
 		// TODO verallgemeinern falls mehr als 6 Knoten möglich gemacht werden sollen
 		for(int i = 0; i < input.length(); i++)
-			if(input.charAt(i) > 'F')
+			if(input.charAt(i) > maxC)
 				return false;
 		
 		return true;
@@ -149,7 +202,7 @@ public class Frame extends JFrame implements ActionListener{
 		input = input.replaceAll(",", "");
 		
 		// TODO replace F with highest NODE letter -> Obergrenze variabel machen
-		for(char c = 'A'; c <= 'F'; c++)
+		for(char c = 'A'; c <= maxC; c++)
 		{
 			if((c != tbNum) && (input.contains(Character.toString(c))))
 				if(!textFields[c].getText().contains(Character.toString(tbNum)))
--- TextBoxSnippet/src/de/yanik/start/Starter.java
+++ TextBoxSnippet/src/de/yanik/start/Starter.java
@@ -5,7 +5,7 @@ import de.yanik.programm.Frame;
 public class Starter {
 
 	public static void main(String[] args) {
-		new Frame();
+		new Frame(4);
 	}
 
 }
